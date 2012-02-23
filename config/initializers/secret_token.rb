9442c03853433088a53f1bf6b6186862c115ed99
Austin Fonacier <austin@spokeo.com>
2012-02-23 13:47:03 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'ad2ef7547076d584f5c6c3679f74dcc532175c248c0ff5d5f4605c03110626432bc39c6b4b9e0d5be189904cbd526516d805cf4b03697d3e39c005c8de8b015c'
